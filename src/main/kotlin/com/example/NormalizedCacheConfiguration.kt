4dfef6af8bd1082dff7f311c0606f44c44772daa
BoD <BoD@JRAF.org>
2022-12-09 16:05:28 +0100

    NormalizedCacheConfiguration

--- src/main/kotlin/com/example/NormalizedCacheConfiguration.kt
+++ src/main/kotlin/com/example/NormalizedCacheConfiguration.kt
@@ -3,13 +3,34 @@ package com.example
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.cache.normalized.lru.EvictionPolicy
 import com.apollographql.apollo.cache.normalized.lru.LruNormalizedCacheFactory
+import java.util.concurrent.TimeUnit
 
 suspend fun main() {
-    val cacheFactory = LruNormalizedCacheFactory(
+    val cacheFactory1 = LruNormalizedCacheFactory(
         evictionPolicy = EvictionPolicy.builder().maxSizeBytes(10 * 1024 * 1024).build()
+    )
+    val cacheFactory2 = LruNormalizedCacheFactory(
+        EvictionPolicy.builder().maxSizeBytes(10 * 1024 * 1024).build()
+    )
+    val cacheFactory3 = LruNormalizedCacheFactory(
+        EvictionPolicy.builder()
+            .maxSizeBytes(10 * 1024 * 1024)
+            .expireAfterWrite(10, TimeUnit.MILLISECONDS)
+            .build()
+    )
+    val cacheFactory4 = LruNormalizedCacheFactory(
+        EvictionPolicy.builder()
+            .maxSizeBytes(10 * 1024 * 1024)
+            .expireAfterWrite(10, TimeUnit.MILLISECONDS)
+            .expireAfterAccess(10, TimeUnit.MILLISECONDS)
+            .maxEntries(42)
+            .build()
     )
 
     val apolloClient = ApolloClient.builder()
-        .normalizedCache(cacheFactory)
+        .normalizedCache(cacheFactory1)
+        .normalizedCache(cacheFactory2)
+        .normalizedCache(cacheFactory3)
+        .normalizedCache(cacheFactory4)
         .build()
 }
